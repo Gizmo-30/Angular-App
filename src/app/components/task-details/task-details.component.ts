3e4afc0af4816d9d127abe420313602fdff1d11a
Anvar Adizov <anvaradizov100@gmail.com>
2024-04-16 18:02:00 +0500

    before second nav

--- src/app/components/task-details/task-details.component.ts
+++ src/app/components/task-details/task-details.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 import {MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
 import {MatIcon} from "@angular/material/icon";
 import {MatInput} from "@angular/material/input";
+import {
+  MatCell, MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef,
+  MatRow, MatRowDef,
+  MatTable
+} from "@angular/material/table";
 
 @Component({
   selector: 'app-task-details',
@@ -11,7 +20,17 @@ import {MatInput} from "@angular/material/input";
     MatIcon,
     MatLabel,
     MatHint,
-    MatInput
+    MatInput,
+    MatTable,
+    MatRow,
+    MatHeaderCell,
+    MatColumnDef,
+    MatCell,
+    MatHeaderRow,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRowDef,
+    MatRowDef
   ],
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.sass'
